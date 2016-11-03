5a3d204e10c96e7b45cf9740a5283c0939d8b2ee
mares cares <mares cares>
2016-11-03 15:41:01 +0100

    setting up php-fpm - log file permission error

--- provisioners/bootstrap.sh
+++ provisioners/bootstrap.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-#rpm -ivh http://nginx.org/packages/centos/7/noarch/RPMS/nginx-release-centos-7-0.el7.ngx.noarch.rpm
-yum update
-yum install -y httpd mariadb-server mariadb nginx
-#php php-fpm nginx
+yum -y install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+yum-config-manager --enable remi-php56
+yum -y update
+yum install -y httpd mariadb-server mariadb nginx php php-fpm
 # yum install -y tmux git gitflow tig htop gcc gcc-c++ make ntp wget rsync
 if ! [ -L /var/www ]; then
   rm -rf /var/www
@@ -15,27 +15,31 @@ fi
 #mysqld --initialize-insecure --user=mysql
 
 # Configure PHP
-#mv /etc/php-fpm.d/www.conf /etc/php-fpm.d/www.conf.orig
+mv /etc/php-fpm.d/www.conf /etc/php-fpm.d/www.conf.orig
 
 # Configure nginx
 #semanage permissive -a httpd_t
+
+# echo "Include /opt/code/boxes/centos7/apache/*.conf" >> /etc/httpd/conf/httpd.conf
+rm /etc/httpd/conf/httpd.conf
+cp /opt/code/boxes/centos7/apache/httpd.conf.b /etc/httpd/conf/httpd.conf
 
 # Enable and start services
 systemctl daemon-reload
 systemctl enable httpd
 #systemctl enable memcached.service
 systemctl enable mariadb
-#systemctl enable php-fpm.service
+systemctl enable php-fpm.service
 systemctl enable nginx
 
 systemctl start httpd
 #systemctl start memcached.service
 systemctl start mariadb
-#systemctl start php-fpm.service
+systemctl start php-fpm.service
 systemctl start nginx
 
 # Create the remote MySQL user
-# mysql -e "create user 'root'@'10.%'"
-# mysql -e "create user 'root'@'150.10.10.%'"
-# mysql -e "grant all privileges on *.* to 'root'@'10.%'"
-# mysql -e "grant all privileges on *.* to 'root'@'150.10.10.%'"
+mysql -e "create user 'root'@'10.%'"
+mysql -e "create user 'root'@'150.10.10.%'"
+mysql -e "grant all privileges on *.* to 'root'@'10.%'"
+mysql -e "grant all privileges on *.* to 'root'@'150.10.10.%'"
