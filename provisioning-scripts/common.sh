5a572e2fa327e51d9abca01de11aa3e26e719077
Marko Marjanovic <marko.marjanovic@stackpath.com>
2016-11-16 21:50:33 +0100

    remove comments

--- provisioning-scripts/common.sh
+++ provisioning-scripts/common.sh
@@ -5,7 +5,6 @@ yum -y update
 curl -o /etc/yum.repos.d/dperson-neovim-epel-7.repo https://copr.fedorainfracloud.org/coprs/dperson/neovim/repo/epel-7/dperson-neovim-epel-7.repo
 
 # Install general tools, daemons, and build tools.
-#yum -y install vim-enhanced tmux git gitflow tig htop policycoreutils-python the_silver_searcher gcc gcc-c++ make ntp wget rsync
 yum -y install rsync vim-enhanced tmux policycoreutils-python git ntp wget make
 
 # Double check the system time.
